0d4945caf83e683f6d1bb3e5e40ec7634a292ce5
HelmoFilho <helmofilho09@gmail.com>
2021-08-22 20:26:24 -0300

    all jobs in the page

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personaliz
 html_text = requests.get(url).text
 soup = BeautifulSoup(html_text, "lxml")
 
-job = soup.find("li", class_ = "clearfix job-bx wht-shd-bx")
+jobs = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
 
-job_comp = job.find("h3", class_ = "joblist-comp-name").text
-job_comp = re.sub(r"\s\s", "", job_comp)
+for job in jobs:
 
-job_skills = job.find("span", class_ = "srp-skills").text
-job_skills = re.sub(r"\s\s", "", job_skills)
-print(job_skills)
+    job_comp = job.find("h3", class_ = "joblist-comp-name").text
+    job_comp = re.sub(r"\s\s", "", job_comp)
+
+    job_skills = job.find("span", class_ = "srp-skills").text
+    job_skills = re.sub(r"(\s\s|\n)", "", job_skills)
+
+    job_date = job.find("span", class_ = "sim-posted").span.text
+    job_date = re.sub(r"(\s\s|\n)", "", job_date)
+
+    message = f"""
+Job Company: {job_comp}
+Skills necessary: {job_skills}
+Date of posting: {job_date}"""
+
+    print(message)
